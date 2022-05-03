1b8baf83a9933986c5deb944d1b36fb1cfe67158
abhikumar11 <abhishekkumar12verma@gmail.com>
2022-05-03 10:16:18 +0530

    initial commit

--- cal/src/component/ExpenseItem.js
+++ cal/src/component/ExpenseItem.js
@@ -1,14 +1,17 @@
-import react from "react";
-import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
-function ExpenseItem(props)
-{
-  return(
-    <div className="expense-item">
-      <ExpenseDate date={props.date}/>
-    <div className="expense-item__description"><h2>{props.title}</h2></div>
-    <div className="expense-item__price"><h2>${props.price}</h2></div>
-    </div>
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
   );
 }
+
 export default ExpenseItem;
