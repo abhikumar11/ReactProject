fb1e6623a3b289c24420b49a56081526c3c30fce
abhikumar11 <abhishekkumar12verma@gmail.com>
2022-05-03 08:17:19 +0530

    initial commit

--- cal/src/App.js
+++ cal/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './component/ExpenseItem';
+import './App.css'; 
+import Expense from './component/Expense';
 function App() {
   const expenses=[
     {title:"Car Insurance",amount:200,date:new Date(2022,4,2)},
@@ -9,9 +9,7 @@ function App() {
   ];
   return (
     <div className="App">
-     <ExpenseItem title={expenses[0].title} date={expenses[0].date} price={expenses[0].amount}/>
-     <ExpenseItem title={expenses[1].title} date={expenses[1].date} price={expenses[1].amount}/>
-     <ExpenseItem title={expenses[2].title} date={expenses[2].date} price={expenses[2].amount}/>
+    <Expense expenses={expenses}/>
     </div>
   );
 }
